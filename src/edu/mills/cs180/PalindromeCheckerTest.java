64550d8399e693f0a60bda37950b79f1174c8e65
Jennifer Flores <jeflores@mills.edu>
2022-03-14 06:52:57 -0700

    Add test class

---
+++ src/edu/mills/cs180/PalindromeCheckerTest.java
@@ -0,0 +1,13 @@
+package edu.mills.cs180;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class PalindromeCheckerTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
